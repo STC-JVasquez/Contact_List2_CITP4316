886d1a3840dc71f1e4d1490b34b2c95f9b973e00
Juan Vasquez <jvasqu53@stu.southtexascollege.edu>
2024-09-14 22:05:40 -0500

    created form and added name textbox

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
-
+import {ref} from 'vue'
 </script>
 
 <template>
   <main class="app">
-    <section class="greeting">
+    <section class="contact-form">
+      <h2> Contact list</h2>
+      <form @submit.prevent="addContact">
+        <div>
+          <label for="name">Name:</label>
+          <input type="text" id="name" v-model="name" placeholder="Enter name" />
+        </div>
+      </form>
 
     </section>
   </main>
